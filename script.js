c2913f4761420cd447e3e6ca9ef1b8e684ce325a
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-04-26 16:29:08 +0200

    starting

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+document.getElementById("pa").addEventListener("click", function() {
+    pa.style.transition = "2s";
+    pa.style.transform = "translateX(100vw)";
+});
+
+});
