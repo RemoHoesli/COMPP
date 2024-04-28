2c3d7b18a45b4f72bc5b65a4d71b6fd3bf1af7ce
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-04-28 06:43:46 +0200

    day2

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-document.getElementById("pa").addEventListener("click", function() {
-    pa.style.transition = "2s";
-    pa.style.transform = "translateX(100vw)";
-});
+
+    document.getElementById("button").addEventListener("click", function() {
+        const opa = document.getElementById("opa");
+        const video = document.getElementById("video");
+        const sound = document.getElementById("sound");
+        button.style.transition = "2s";
+        button.style.opacity = "0%";
+        opa.style.transition = "2s";
+        opa.style.opacity = "0%";
+        video.play();
+        sound.volume = 0.2;
+        sound.play();
+    });
 
 });
