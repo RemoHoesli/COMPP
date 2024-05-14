e9754e5166687d9ac52b2984a09556995bee4d93
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-05-14 10:51:10 +0200

    something doesn't seem to be working

--- script.js
+++ script.js
@@ -20,12 +20,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById("button").addEventListener("click", function() {
         const opa = document.getElementById("opa");
+        const circle = document.getElementById("circle");
+        const smolcircle = document.getElementById("smolcircle");
+        const jumpcircle = document.getElementById("jumpcircle");
         const video = document.getElementById("video");
         const sound = document.getElementById("sound");
         button.style.transition = "2s";
         button.style.opacity = "0%";
         opa.style.transition = "2s";
         opa.style.opacity = "0%";
+
         //video.play();
         sound.volume = 0.2;
         //sound.play();
